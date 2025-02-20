cc10aa0aa18c1968dd2f039d43ba37e8bcc19082
Roman Tolstosheyev <32099652+IZOBRETATEL777@users.noreply.github.com>
2025-02-20 15:40:54 +0400

    Update vuln.ts

--- vuln.ts
+++ vuln.ts
@@ -10,3 +10,10 @@ console.log(`Insecure random number: ${insecureRandom}`);
 
 const code = `console.log("Executing arbitrary code!");`;
 eval(code);
+
+
+import { executeQuery } from 'database';
+
+const userId = '1; DROP TABLE users;';
+const query = `SELECT * FROM users WHERE id = ${userId}`;
+executeQuery(query);
